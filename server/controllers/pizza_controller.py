6482903711a22de9f71d23bebe64344fa6e1eabe
Larrymeez <meezlarry@gmail.com>
2025-06-17 20:26:22 +0300

    Register blueprints and initialize Flask app with SQLAlchemy and Migrate

--- server/controllers/pizza_controller.py
+++ server/controllers/pizza_controller.py
@@ -6,7 +6,8 @@ pizza_bp = Blueprint('pizza_bp', __name__)
 @pizza_bp.route('/pizzas', methods=['GET'])
 def get_pizzas():
     pizzas = Pizza.query.all()
-    return jsonify([
-        {'id': p.id, 'name': p.name, 'ingredients': p.ingredients}
-        for p in pizzas
-    ])
+    return jsonify([{
+        "id": p.id,
+        "name": p.name,
+        "ingredients": p.ingredients
+    } for p in pizzas]), 200
--- server/controllers/restaurant_pizza_controller.py
+++ server/controllers/restaurant_pizza_controller.py
@@ -1,31 +1,41 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, jsonify, request
 from server.app import db
 from server.models.restaurant_pizza import RestaurantPizza
 from server.models.pizza import Pizza
+from server.models.restaurant import Restaurant
 
 restaurant_pizza_bp = Blueprint('restaurant_pizza_bp', __name__)
 
 @restaurant_pizza_bp.route('/restaurant_pizzas', methods=['POST'])
-def add_restaurant_pizza():
-    data = request.get_json()
-    price = data.get('price')
-    pizza_id = data.get('pizza_id')
-    restaurant_id = data.get('restaurant_id')
+def create_restaurant_pizza():
+    try:
+        data = request.get_json()
+        price = data['price']
+        pizza_id = data['pizza_id']
+        restaurant_id = data['restaurant_id']
 
-    errors = []
-    if price is None or not (1 <= price <= 30):
-        errors.append('Price must be between 1 and 30.')
-    if not pizza_id:
-        errors.append('pizza_id is required.')
-    if not restaurant_id:
-        errors.append('restaurant_id is required.')
+        rp = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
+        db.session.add(rp)
+        db.session.commit()
 
-    if errors:
-        return jsonify({'errors': errors}), 400
+        return jsonify({
+            "id": rp.id,
+            "price": rp.price,
+            "pizza_id": rp.pizza.id,
+            "restaurant_id": rp.restaurant.id,
+            "pizza": {
+                "id": rp.pizza.id,
+                "name": rp.pizza.name,
+                "ingredients": rp.pizza.ingredients
+            },
+            "restaurant": {
+                "id": rp.restaurant.id,
+                "name": rp.restaurant.name,
+                "address": rp.restaurant.address
+            }
+        }), 201
 
-    rp = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
-    db.session.add(rp)
-    db.session.commit()
-
-    pizza = Pizza.query.get(pizza_id)
-    return jsonify({'id': pizza.id, 'name': pizza.name, 'ingredients': pizza.ingredients}), 201
+    except ValueError as e:
+        return jsonify({"errors": [str(e)]}), 400
+    except Exception:
+        return jsonify({"errors": ["Invalid input or foreign keys not found"]}), 400
